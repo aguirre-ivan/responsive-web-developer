dd58b16b21496f97366f6a30bcb3c53e668ff8b7
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-10 21:10:42 -0300

    Added colormode to menu

--- simple-homepage/js/app.js
+++ simple-homepage/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $("#toggleMenu").click(function() {
+        $("body").toggleClass('no-scroll');
         $(".burger-icon").toggle();
         $(".close-icon").toggle();
-        $("header nav").toggleClass('show')
+        $("header nav").toggleClass('show');
+        $("#colorMode").toggleClass('show');
         $("header nav").css("opacity", function() {
             return $(this).css("opacity") === "0" ? "1" : "0";
         });
